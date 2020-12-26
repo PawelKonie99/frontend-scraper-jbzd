1956c64eb5556a21042c3c461f694d5d28c61eff
PawelKonie99 <pawel.konieczny21@gmail.com>
2020-12-26 21:20:58 +0100

    correct choosen meme css

--- src/components/ChoosenMeme/ChoosenMemeElements.tsx
+++ src/components/ChoosenMeme/ChoosenMemeElements.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const MemeContainer = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-bottom: 0;
   background: ${({ theme }) => theme.colors.background};
+  padding: 4rem 0;
 `;
